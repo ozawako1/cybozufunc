049d56e921fb2848c7fbc544837d1bfcdf39d7cd
ozawako1 <ozawako1@gmail.com>
2020-04-29 17:20:44 +0900

    add email

--- GetZoomFacilities/index.js
+++ GetZoomFacilities/index.js
@@ -2,6 +2,19 @@
 var rp = require('request-promise');
 var fs = require('fs');
 
+const REG_FORMAT = /motex_zoom\d{1,2}@motex\.co\.jp/;
+
+function get_email_address(notes){
+    var email = "";
+
+    var tmp = notes.match(REG_FORMAT);
+    if (tmp != null){
+        email = tmp[0];
+    }
+
+    return email;
+}
+
 module.exports = function (context, req) {
 
     context.log('GetZoomFacilities was triggered!');
@@ -27,7 +40,10 @@ module.exports = function (context, req) {
     rp(options)
     .then((body)    => {
         //zoomをnameに含む設備のみ抽出
-        var arr = body.facilities.filter(facility => (facility.name.toLowerCase().indexOf("zoom") != -1));        
+        var arr = body.facilities.filter(facility => (facility.name.toLowerCase().indexOf("zoom") != -1));  
+        arr.forEach(function(fa){
+            fa.email = get_email_address(fa.notes);
+        });      
         context.res = {
             'status': 200,
             'content-type': 'application/json',
